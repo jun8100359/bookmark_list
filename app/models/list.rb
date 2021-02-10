c6d968604fff4f5ed11eebe9cb5e7a98aa3371ff
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-02-10 19:35:18 +0900

    バリデーション、アソシエーション記述

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
+  has_many :bookmarks
 
   validates :list_name, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   has_many :lists
+  has_many :bookmarks
 
 end
