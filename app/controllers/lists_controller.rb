600559d312f5101bfe697d668d86d34894646fec
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-02-10 15:04:14 +0900

    コントローラーに保存の処理

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,26 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.order('created_at DESC')
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.valid?
+      @list.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def list_params
+    params.require(:list).permit(:list_name).merge(user_id: current_user.id)
+  end
+
 end
